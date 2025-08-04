8070d7d479edc2e9a03b46c6caad0a17be40cd3c
Keen Sha <krishna00rads11@gmail.com>
2025-08-04 23:22:22 +0545

    goodnight commit

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import cors from "cors";
 import 'dotenv/config';
 import cookieParser from "cookie-parser";
 import connectDB from './config/mongodb.js';
-
+import authRouter from './routes/authRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -14,8 +14,9 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({credentials: true}));
 
+// API ENDPOINTS
 app.get('/', (req, res) => res.send("API Working Fine"));
-
+app.use('/api/auth', authRouter)
 
 app.listen(port, () => console.log(`Server is running on PORT:${port}`));
 
